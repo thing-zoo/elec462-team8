d0d6fb4ec0e3afd655e7c80984fa13c923ca1ee3
thing-zoo <cathy4025@gmail.com>
2020-12-13 15:15:48 +0900

    clean

--- serverlib.h
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <string.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <fcntl.h>
-
-int sign_up(char*, char*); //sign up
-int check(char*); //check id already exist 
-int log_in(char*, char*); //log in
